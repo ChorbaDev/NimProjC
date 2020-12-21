8389c24e93486ba37983f06a27fcfc314ce997d8
Blue <leanefelis@gmail.com>
2020-12-21 16:37:35 +0100

    update

--- nim.h
+++ nim.h
@@ -15,22 +15,31 @@ struct Case{
   int C;
 } ;
 typedef struct Case T_Case;
+
 //Un tableau de ce type permettra de stocker l’ensemble des voisines d’une case donnée.
 typedef T_Case T_Tab_Case;
+
 //une fonction permet de saisir et de retourner un entier compris entre deux bornes.
 int Lire_Entier(int BINF,int BSUP);
+
 //fonction qui calcule le nimber d'une case donnee
 int Nimber(T_Case pion,int m[VMAX][VMAX]);
+
 //permet de saisir des donnees
 void Parametres(int *nlig,int *ncol,int *niveau,int *next);
+
 //construire le matrice qui contient les nimbers
 void grille_nimber(int m[VMAX][VMAX],int nlig,int ncol);
+
 //fonction qui gère le coup d’un joueur
 int Coup_joueur (T_Tab_Case t[],int nb_vois);
+
 //fonction qui gère le coup gagnant effectué par l’ordinateur
 int Coup_Ordi_Gagnant(T_Case p,T_Tab_Case t[],int n,int m[VMAX][VMAX]);
+
 //une fonction qui construit la table des cases voisines d’une case donnée
 void Voisines (T_Case pion,T_Tab_Case t[],int *nb_vois,int nlig,int ncol);
+
 //affichage du grille
 void affiche_grille(T_Case pion,int nlig,int ncol);
 #endif
