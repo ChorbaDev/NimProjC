d44a5084223c8d0032d7d81de366c318ffa09b6f
OmareEll <omarelloumi530@gmail.com>
2020-12-24 14:49:53 +0100

    updates

--- main.c
+++ main.c
@@ -12,8 +12,9 @@ T_Tab_Case tab_vois[4];
 int coup,nb_vois,nv_pos,grille[VMAX][VMAX];
 int Equivalent;
 grille_nimber(grille,nlig,ncol);
-if (next==2)  coup=1;
-else coup=-1;
+  if (next==2)  coup=1;
+  else coup=-1;
+//
   do{
       affiche_grille(pion,nlig,ncol);
       Voisines (pion,tab_vois,&nb_vois,nlig,ncol);
@@ -29,6 +30,7 @@ else coup=-1;
       coup=-1*coup;
       Equivalent=(dernier_pion.Ligne==pion.Ligne)&&(dernier_pion.Colonne==pion.Colonne);
     }while (!Equivalent);
+//
     affiche_grille(pion,nlig,ncol);
     printf("C est termine. ");
     if(coup==-1) printf("BRAVO TU AS GAGNE");
