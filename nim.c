d6347d796075f1a242e13ab3b90363f8f46623f8
Blue <leanefelis@gmail.com>
2020-12-30 18:05:51 +0100

    scanf

--- nim.c
+++ nim.c
@@ -123,7 +123,7 @@ int Lire_Entier(int BINF,int BSUP)
   int entier = 0;
   do {
   printf("Saisir un entier compris entre %d et %d\n", BINF, BSUP); //on demande la saisie de l'entier et on affiche l'intervalle
-  scanf(entier); //on lit l'entier
+  scanf("%d",&entier); //on lit l'entier
   } while (entier < BINF || entier > BSUP);
   return 0;
 }
