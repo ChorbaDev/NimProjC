dac15cc1ad24f197ace4f9d4f3bf70c12ec8628e
OmareEll <omarelloumi530@gmail.com>
2021-01-03 15:37:09 +0100

    trash

--- nim.c
+++ nim.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-//fonction qui permet de remplir la grille par des nimbers
-/*void grille_nimber(int m[VMAX][VMAX],int nlig,int ncol){
-int i,j,x=0;
-  for(i=ncol-1;i>=0;i--){
-    for(j=nlig-1;j>=0;j--){
-      m[j][i]=(x==0)?0:1;
-      x=(x==2)?0:x+1;
-    }
-  }
-}
-//fonction qui calcule le nimber d'une case donnee
-int Nimber(T_Case pion,int m[VMAX][VMAX]){
-  int i=pion.Ligne-1,j=pion.Colonne-1;
-  return m[i][j];
-}*/
 //fonction qui trouve les voisines
 void Voisines(T_Case pion, T_Tab_Case tab_vois[],int *nb_vois,int nlig,int ncol)
 {
--- nim.h
+++ nim.h
@@ -23,12 +23,8 @@ typedef struct Case T_Case;
 typedef T_Case T_Tab_Case;
 //une fonction permet de saisir et de retourner un entier compris entre deux bornes.
 int Lire_Entier(int BINF,int BSUP);
-//fonction qui calcule le nimber d'une case donnee
-int trouve_Nimber(T_Case pion,int grille[VMAX][VMAX]);
 //permet de saisir des donnees
 void Parametres(int *nlig,int *ncol,int *niveau,int *next);
-//construire le matrice qui contient les nimbers
-void grille_nimber(int grille[VMAX][VMAX],int nlig,int ncol);
 //fonction qui gère le coup d’un joueur
 int Coup_joueur (T_Tab_Case tab_vois[],int nb_vois);
 //fonction qui gère le coup gagnant effectué par l’ordinateur
@@ -38,7 +34,7 @@ void Voisines (T_Case pion,T_Tab_Case tab_vois[],int *nb_vois,int nlig,int ncol)
 //affichage du grille
 void affiche_grille(T_Case pion,int nlig,int ncol);
 //retourne la position dans le tableau des voisines dont l'ordinateur a choisie
-int Coup_Ordi(int niveau,T_Case pion,T_Tab_Case tab_vois[],int nb_vois,int nlig,int ncol);
+int Coup_Ordi(int niveau,T_Case pi,T_Tab_Case tab_vois[],int nb_vois,int nlig,int ncol);
 //pour trouver le Nimber d'un coodonne donnee
 int Nimber(T_Case pion,int nlig,int ncol);
 #endif
